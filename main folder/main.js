2666ea10e64dc40585fd29710528d7598f0d0cfd
orilivne55 <orior200@gmail.com>
2023-09-27 15:57:58 +0300

    wether API

--- main folder/main.js
+++ main folder/main.js
@@ -15,22 +15,44 @@
         
 
 
-        function fetchData() {
-                  fetch('https://api.chucknorris.io/jokes/random')
-        .then(response => {
-          console.log(response);
-          return response.json()
-        })
-        .then(data => {
-          console.log(data);
+        // function fetchData() {
+        //           fetch('https://api.chucknorris.io/jokes/random')
+        // .then(response => {
+        //   console.log(response);
+        //   return response.json()
+        // })
+        // .then(data => {
+        //   console.log(data);
+        //   const mainDiv = document.getElementById("mainDiv")
+        //   const btn = document.getElementById("btn")
+        //   mainDiv.innerHTML = 
+        //     `<h5>${data.value}</h5>`
+        //     btn.addEventListener("click",fetchData)
+        // })
+        // .catch(error => {
+        //   console.log(error);
+        // })
+        // }
+        // fetchData()
+
+
+        function weatherApi(city) {
           const mainDiv = document.getElementById("mainDiv")
-          const btn = document.getElementById("btn")
-          mainDiv.innerHTML = 
-            `<h5>${data.value}</h5>`
-            btn.addEventListener("click",fetchData)
-        })
-        .catch(error => {
-          console.log(error);
-        })
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7ec52230bc0f1f72539d3ee739a55306
+          `
+          fetch(url)
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            mainDiv.innerHTML += `<p> today temp: ${data.main.temp}</p>
+            <p> feels like: ${data.main.feels_like}</p>
+            `
+          })
         }
-        fetchData()
+        btn2.addEventListener("click", (e) =>{
+          e.preventDefault()
+          const enterCity = document.getElementById("enterCity")
+          let city = enterCity.value
+          weatherApi(city)
+        })
+        
