860dfd14205578b0041d563595a823dd5173c5cb
orilivne55 <orior200@gmail.com>
2023-09-28 09:32:25 +0300

    icon :)

--- main folder/main.js
+++ main folder/main.js
@@ -36,23 +36,64 @@
         // fetchData()
 
 
+        // function weatherApi(city) {
+        //   const mainDiv = document.getElementById("mainDiv")
+        //   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7ec52230bc0f1f72539d3ee739a55306
+        //   `
+        //   fetch(url)
+        //   .then(res => res.json())
+        //   .then(data => {
+        //     console.log(data);
+        //     mainDiv.innerHTML += `<p> today temp: ${data.main.temp}</p>
+        //     <p> feels like: ${data.main.feels_like}</p>
+        //     `
+        //   })
+        // }
+        // btn2.addEventListener("click", (e) =>{
+        //   e.preventDefault()
+        //   const enterCity = document.getElementById("enterCity")
+        //   let city = enterCity.value
+        //   weatherApi(city)
+        // })
+        
+
         function weatherApi(city) {
           const mainDiv = document.getElementById("mainDiv")
-          const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7ec52230bc0f1f72539d3ee739a55306
-          `
+          const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=7ec52230bc0f1f72539d3ee739a55306`
           fetch(url)
           .then(res => res.json())
           .then(data => {
             console.log(data);
-            mainDiv.innerHTML += `<p> today temp: ${data.main.temp}</p>
-            <p> feels like: ${data.main.feels_like}</p>
-            `
+            mainDiv.innerHTML = `<h1>Weather forecast for ${city}</h1>`;
+            let previousDate = null; 
+            for (let i = 0; i < data.list.length; i++) {
+              
+              const iconUrl = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon +".png"
+              const timestamp = data.list[i].dt_txt;
+              const temperature = data.list[i].main.temp;
+      
+              const currentDate = timestamp.substr(0, 10);
+      
+              if (currentDate !== previousDate) {
+                mainDiv.innerHTML += `<h1>Date: ${currentDate}</h1>`;
+                previousDate = currentDate;
+              }
+              mainDiv.innerHTML += `
+                <h3>Time: ${timestamp.substr(11)}</h3>
+                <img src="${iconUrl}">
+                <h4>Temperature: ${temperature}°C</h4>
+              `;
+            }
           })
-        }
+          .catch(error => {
+              console.log(error);
+            })
+            }
+        
+        
         btn2.addEventListener("click", (e) =>{
           e.preventDefault()
           const enterCity = document.getElementById("enterCity")
           let city = enterCity.value
           weatherApi(city)
         })
-        
